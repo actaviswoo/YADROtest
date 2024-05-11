837ad437830fb92f5050014d6e7c8e0c74bda786
argoniaz <argoniaz@student.21-school.ru>
2024-05-11 14:59:24 +0300

    handler ready

--- src/model/Format/Format.cc
+++ src/model/Format/Format.cc
@@ -7,13 +7,62 @@ bool Format::IsValidTime(const std::string& time) {
     return std::regex_match(time, timePattern);
 }
 
+int Format::GetHour(const std::string& time) {
+    return stoi(time.substr(0, 2));
+}
+int Format::GetMinute(const std::string& time) {
+    return stoi(time.substr(3, 2));
+}
+
+std::string Format::SumTime(const std::string& time1, const std::string& time2) {
+    if (time1.empty() || time2.empty()) {
+        return "";
+    }
+    int hour = GetHour(time1) + GetHour(time2);
+    int minute = GetMinute(time1) + GetMinute(time2);
+    if (minute >= 60) {
+        hour++;
+        minute -= 60;
+    }
+    char buffer[6];
+    snprintf(buffer, sizeof(buffer), "%02d:%02d", hour, minute);
+    return buffer;
+}
+
+std::string Format::DiffTime(const std::string& time1, const std::string& time2) {
+    if (time1.empty() || time2.empty()) {
+        return "";
+    }
+    int hour1 = GetHour(time1);
+    int minute1 = GetMinute(time1);
+    int hour2 = GetHour(time2);
+    int minute2 = GetMinute(time2);
+    int diffHour = hour1 - hour2;
+    int diffMinute = minute1 - minute2;
+    if (diffMinute < 0) {
+        diffHour--;
+        diffMinute += 60;
+    }
+    char buffer[6];
+    snprintf(buffer, sizeof(buffer), "%02d:%02d", diffHour, diffMinute);
+    return buffer;
+}
+
+int Format::RoundTime(const std::string& time) {
+    int hour = GetHour(time);
+    int minute = GetMinute(time);
+    if (minute > 0)
+        return hour + 1;
+    return hour;
+}
+
 bool Format::CompareTime(const std::string& time1, const std::string& time2) {
     if (time1.empty() || time2.empty())
         return false;
-    int hour1 = stoi(time1.substr(0, 2));
-    int minute1 = stoi(time1.substr(3, 2));
-    int hour2 = stoi(time2.substr(0, 2));
-    int minute2 = stoi(time2.substr(3, 2));
+    int hour1 = GetHour(time1);
+    int minute1 = GetMinute(time1);
+    int hour2 = GetHour(time2);
+    int minute2 = GetMinute(time2);
     if (hour1 < hour2) {
         return true;
     } else if (hour1 > hour2) {
--- src/model/Format/Format.h
+++ src/model/Format/Format.h
@@ -10,6 +10,11 @@ class Format {
     static bool IsValidTime(const std::string& time);
     static bool CompareTime(const std::string& t1, const std::string& t2);
     static bool IsValidName(const std::string& name); 
+    static int GetHour(const std::string& time);
+    static int GetMinute(const std::string& time);
+    static std::string SumTime(const std::string& time1, const std::string& time2);
+    static std::string DiffTime(const std::string& time1, const std::string& time2);
+    static int RoundTime(const std::string& time);
 };
 
 #endif  // MODEL_FORMAT_H
